85581fb0824e23afa3631235e352f9b4f8637bc3
Jason Bacon <bacon@moray.acadix.biz>
2022-02-03 17:19:11 -0600

    feature-view.py: Cycle through argv, generate right-size plots and pngs

--- feature-view.py
+++ feature-view.py
@@ -20,35 +20,56 @@
 # Quick-start:
 # https://edinburgh-genome-foundry.github.io/DnaFeaturesViewer/
 
-# import sys,os
+import sys,os
 
 import matplotlib.pyplot as plt
 from dna_features_viewer import GraphicFeature, GraphicRecord, CircularGraphicRecord
 from dna_features_viewer import BiopythonTranslator
 from BCBio import GFF
 
+argc = len(sys.argv)
+if ( argc < 2 ):
+    print("Usage:", sys.argv[0], "file.gff3 [file.gff3 ...]\n")
+    exit(1)
+
 # Can build a record manually or use translate_record() as below
-features=[
-    GraphicFeature(start=1000, end=1020, strand=+1, color="#ffd700",
-                   label="Small feature"),
-    GraphicFeature(start=1020, end=1500, strand=+1, color="#ffcccc",
-                   label="Gene 1 with a very long name"),
-    GraphicFeature(start=1400, end=1700, strand=-1, color="#cffccc",
-                   label="Gene 2"),
-    GraphicFeature(start=1600, end=1900, strand=+1, color="#ccccff",
-                   label="Gene 3")
-]
+#features=[
+#    GraphicFeature(start=1000, end=1020, strand=+1, color="#ffd700",
+#                   label="Small feature"),
+#    GraphicFeature(start=1020, end=1500, strand=+1, color="#ffcccc",
+#                   label="Gene 1 with a very long name"),
+#    GraphicFeature(start=1400, end=1700, strand=-1, color="#cffccc",
+#                   label="Gene 2"),
+#    GraphicFeature(start=1600, end=1900, strand=+1, color="#ccccff",
+#                   label="Gene 3")
+#]
 #record = GraphicRecord(sequence_length=2000, features=features)
 #record.plot()
 #plt.show()
 
-file="jun-hood.gff3"
-handle = open(file)
-for rec in GFF.parse(handle):
-    print(rec)
-handle.close()
+for i in range(1, argc):
+    filename = sys.argv[i]
 
-record = BiopythonTranslator().translate_record("jun-hood.gff3", filetype="gff")
-print(record)
-ax, _ = record.plot(x_lim = [15550000, 16000000], strand_in_label_threshold=7)
-plt.show()
+    # Determine boundaries of the whole neighborhood
+    x_min=9999999999999999
+    x_max=0
+    with open(filename) as infile:
+        for line in infile:
+            if line[0] != '#':
+                cols = line.split("\t")
+                start = int(cols[3])
+                if start < x_min:
+                    x_min = start
+                end = int(cols[4])
+                if end > x_max:
+                    x_max = end
+    infile.close()
+    print("Start coordinate =", x_min, "End coordinate = ", x_max)
+    
+    # Convert GFF file to a matplotlib record
+    record = BiopythonTranslator().translate_record(filename, filetype="gff")
+    ax, _ = record.plot(x_lim = [x_min, x_max], strand_in_label_threshold=7)
+    plt.show()
+    stem = os.path.splitext(filename)[0]
+    png_filename = ".".join([stem,"png"])
+    ax.figure.savefig(png_filename, bbox_inches='tight')
