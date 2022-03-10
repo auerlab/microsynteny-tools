1f0c4315daabadb5f022dcbb0194b8cc9e448708
Jason Bacon <bacon@moray.acadix.biz>
2022-03-10 16:06:15 -0600

    intersect.sh: Fix for duplicate genes

--- intersect.sh
+++ intersect.sh
@@ -26,20 +26,25 @@ fi
 make > /dev/null
 for species in Danio_rerio Oryzias_latipes Takifugu_rubripes; do
     for gene in $@; do
-	file=Regions/$species-$gene-*.gff3
-	if [ -e $file ]; then
-	    regen="$regen $file"
-	fi
+	gene_files="Regions/$species-$gene-*.gff3"
+	for file in $gene_files; do
+	    if [ -e $file ]; then
+		regen="$regen $file"
+	    fi
+	done
     done
 done
 for species in Mus_musculus Rattus_norvegicus Homo_sapiens; do
     for gene in $@; do
-	file=Regions/$species-$gene-*.gff3
-	if [ -e $file ]; then
-	    noregen="$noregen $file"
-	fi
+	gene_files="Regions/$species-$gene-*.gff3"
+	for file in $gene_files; do
+	    if [ -e $file ]; then
+		noregen="$noregen $file"
+	    fi
+	done
     done
 done
+echo $regen $noregen
 
 if [ $(echo $regen | wc -w) -ge 2 ]; then
     printf "\n====================\n"
