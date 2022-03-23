66064e3821b6064cdb782a8b66a8043fbfb84808
Jason Bacon <bacon@moray.acadix.biz>
2022-03-22 19:09:32 -0500

    Generate new gene lists from ID lists

--- Genes/Orig/extract.sh
+++ Genes/Orig/extract.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -e
 
-awk '{ print $2 }' detf-refined.tsv > ../detf-refined-names.txt
+awk '{ print $2 }' detf-refined.tsv > ../detf-refined.txt
 
 awk -F , 'length($2) > 0 && $1 ~ "ENSDART" { print $2 }' all-de.csv \
-    | awk -f split-gene.awk > ../all-de-names.txt
+    | awk -f split-gene.awk > ../all-de.txt
 
+blt ensemblid2gene \
+    ../../GFF/Danio_rerio.GRCz11.105.chr.gff3 detf-ids.txt \
+    | awk '{ print $2 }' | sort | uniq > ../detf.txt
+
+blt ensemblid2gene \
+    ../../GFF/Danio_rerio.GRCz11.105.chr.gff3 expressed_NDE_transcripts.txt \
+    | awk '{ print $2 }' | sort | uniq > ../non-de.txt
