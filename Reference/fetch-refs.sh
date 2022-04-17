a7cd8ea583f3528f41425a193e668951d098cc4b
Jason Wayne Bacon <bacon@barracuda.uits.uwm.edu>
2022-04-17 09:22:27 -0500

    Clean up

--- Reference/fetch-refs.sh
+++ Reference/fetch-refs.sh
@@ -13,16 +13,16 @@ site="http://ftp.ensembl.org/pub/release-$(../Utils/ensembl-release.sh)/fasta"
 # Remove non-chromosomal sequences from toplevel to get primary_assembly.
 ref_type="toplevel"
 for ref in \
-    danio_rerio/dna/Danio_rerio.GRCz11.dna.$ref_type.fa.gz \
-    oryzias_latipes/dna/Oryzias_latipes.ASM223467v1.dna.$ref_type.fa.gz \
-    takifugu_rubripes/dna/Takifugu_rubripes.fTakRub1.2.dna.$ref_type.fa.gz \
-    mus_musculus/dna/Mus_musculus.GRCm39.dna.$ref_type.fa.gz \
-    rattus_norvegicus/dna/Rattus_norvegicus.mRatBN7.2.dna.$ref_type.fa.gz \
-    homo_sapiens/dna/Homo_sapiens.GRCh38.dna.$ref_type.fa.gz
+    danio_rerio/dna/Danio_rerio.GRCz11.dna.$ref_type.fa \
+    oryzias_latipes/dna/Oryzias_latipes.ASM223467v1.dna.$ref_type.fa \
+    takifugu_rubripes/dna/Takifugu_rubripes.fTakRub1.2.dna.$ref_type.fa \
+    mus_musculus/dna/Mus_musculus.GRCm39.dna.$ref_type.fa \
+    rattus_norvegicus/dna/Rattus_norvegicus.mRatBN7.2.dna.$ref_type.fa \
+    homo_sapiens/dna/Homo_sapiens.GRCh38.dna.$ref_type.fa
 do
     base=$(basename $ref)
     printf "===\n"
-    printf "Downloading $base.gz...\n"
-    url=$site/$ref
+    printf "Downloading $base...\n"
+    url=$site/$ref.gz
     curl $curl_flags $url
 done
